a0bc62c9f2ccab07dcb6564361586611e058fd81
Vinayak Borkar <vinayakb@gmail.com>
2026-07-10 23:31:07 -0700

    Fix CI for Dependabot pull requests (#11)
    * Fix CI for Dependabot pull requests
    
    Signed-off-by: Vinayak Borkar <vinayakb@gmail.com>
    
    * Vendor protoc for reproducible builds
    
    Signed-off-by: Vinayak Borkar <vinayakb@gmail.com>
    
    ---------
    
    Signed-off-by: Vinayak Borkar <vinayakb@gmail.com>

--- crates/pepper-network/build.rs
+++ crates/pepper-network/build.rs
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-    prost_build::compile_protos(&["../../proto/pepper/v1/network.proto"], &["../../proto"])
+    let protoc = protoc_bin_vendored::protoc_bin_path()
+        .expect("failed to locate the vendored protoc binary");
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc);
+    config
+        .compile_protos(&["../../proto/pepper/v1/network.proto"], &["../../proto"])
         .expect("failed to compile Pepper network protobuf schema");
 }
